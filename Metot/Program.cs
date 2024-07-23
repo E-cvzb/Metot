5e2e4b2c2bc7baf1ebae15c58e88b32440d2d33b
Emrullah Cevizbaş <cevizbas01@gmail.com>
2024-07-23 14:15:47 +0300

    Proje dosyası ekle.

---
+++ Metot/Program.cs
@@ -0,0 +1,19 @@
+﻿static void şarkı()
+{
+    Console.WriteLine("Elinde sadece siyah kalem var ama gökkuşağı yarat.");
+}
+
+static int kalan( int a )
+{
+    return a % 2;
+}
+
+static int çarpım( int a,int b)
+{
+    return  a * b;
+}
+
+static void kullanıcı (string name, string lastname)
+{
+    Console.WriteLine("Hoşgeldiniz" + name+ lastname );
+}
